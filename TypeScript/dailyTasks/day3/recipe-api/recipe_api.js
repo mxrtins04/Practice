4e99a41728cd16a1eec40537ce0fbd2613abd724
mxrtins04 <olatunbosunmartins14@gmail.com>
2026-01-29 00:36:21 +0100

    all get and post endpoints for recipe API complete

--- TypeScript/dailyTasks/day3/recipe-api/recipe_api.js
+++ TypeScript/dailyTasks/day3/recipe-api/recipe_api.js
@@ -1,11 +1,37 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var app = (0, express_1.default)();
+var express = require('express');
+var app = express();
+app.use(express.json());
 var recipes = [];
 app.get("/", function (req, res) {
-    res.send("Say Hi");
+    res.send("Hiii");
 });
 app.listen(5000, function () {
     console.log("Server is running on port 5000");
+});
+app.post("/recipes", function (req, res) {
+    var _a = req.body, title = _a.title, ingredients = _a.ingredients, instructions = _a.instructions;
+    if (!title || !instructions) {
+        return res.status(400).json({
+            error: 'Missing required fields: name and instructions are required'
+        });
+    }
+    if (!ingredients || !Array.isArray(ingredients) || ingredients.length === 0) {
+        return res.status(400).json({
+            error: 'Ingredients must be a non-empty array'
+        });
+    }
+    var id = Date.now().toString();
+    var newRecipe = { id: id, title: title, ingredients: ingredients, instructions: instructions };
+    recipes.push(newRecipe);
+    res.status(201).json(newRecipe);
+});
+app.get("/recipes", function (req, res) {
+    res.json(recipes);
+});
+app.get("/recipes/:id", function (req, res) {
+    var id = req.params.id;
+    var recipe = recipes.find(function (r) { return r.id === id; });
+    res.json(recipe);
 });
--- TypeScript/dailyTasks/day3/recipe-api/recipe_api.ts
+++ TypeScript/dailyTasks/day3/recipe-api/recipe_api.ts
@@ -1,20 +1,56 @@
-import express from 'express'
+const express = require('express');
+import { Request, Response } from 'express';
 const app = express();
 
 app.use(express.json());
 interface Recipe {
-    id : number;
+    id : String;
     title : string;
     ingredients : string[];
     instructions : string;
 }
 
+
 let recipes : Recipe[] = []
 
-app.get("/", (req, res) => {
-    res.json({"message": "Say Hi"});
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hiii");
 });
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
 });
+
+app.post("/recipes", (req: Request, res: Response) => {
+    const {title, ingredients, instructions } : Recipe = req.body;
+
+    if (!title || !instructions) {
+    return res.status(400).json({ 
+      error: 'Missing required fields: name and instructions are required' 
+    });
+  }
+
+  if (!ingredients || !Array.isArray(ingredients) || ingredients.length === 0) {
+    return res.status(400).json({ 
+      error: 'Ingredients must be a non-empty array' 
+    });
+  }
+
+    const id: String = Date.now().toString();
+    const newRecipe : Recipe = {id, title, ingredients, instructions};
+    recipes.push(newRecipe);
+    res.status(201).json(newRecipe);
+});
+
+app.get("/recipes", (req: Request, res: Response) => {
+    res.json(recipes);
+});
+
+app.get("/recipes/:id", (req: Request, res: Response) => {
+    req.params= {"id": "1769641724631"}
+    const id = req.params.id;
+    const recipe = recipes.find(r => r.id === id);
+    res.json(recipe);
+});
+
+app.delete("rec")
